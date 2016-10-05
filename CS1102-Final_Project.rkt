7e27003e6340d2ada1852f4aea522f5eb9b1f9ba
Alexander Wurts <ajwurts@wpi.edu>
2016-10-04 22:35:34 -0400

    Working Animation

--- CS1102-Final_Project.rkt
+++ CS1102-Final_Project.rkt
@@ -1,9 +1,10 @@
 ;; Alexander Wurts
 ;; Program redraws the screen after every command
+(require test-engine/racket-gui)
 (require "world-cs1102.rkt")
 ;; a shape is (make-shape symbol posn color)
 ;; acceptable shape types are 'triangle, 'ellipse, and 'rectangle
-(define-struct shape (type posn color width height))
+(define-struct shape (type posn color width height ID))
 
 ;; a delta is (make-delta x y)
 (define-struct delta (x y))
@@ -12,48 +13,46 @@
 (define-struct animation (width height cmds))
 
 ;; a cmd is either
-;; - (make-display-shape-cmd shape)
+;; - (make-dd-cmd shape)
 ;; - (make-delete-shape-cmd shape)
 ;; - (make-move-shape-cmd shape)
 ;; - (make-jump-shape-cmd shape)
 ;; - (make-init-shapes-cmd list[shape])
-;; - (make-move-until-collide (shp1)) ;; a collision is either hitting the edge of the screen or another shape
-;; - (repeat-until-off-screen (cmd)) ;; runs a given cmd until the object touches the side of the screen
-(define-struct display-shape-cmd (shape))
+;; - (define-struct cmds-until-xy-cond (shp1 minx miny maxx maxy cmd))
+(define-struct add-shape-cmd (shape))
 (define-struct delete-shape-cmd (shape))
 (define-struct move-shape-cmd (shape delta))
 (define-struct jump-shape-cmd (shape))
-(define-struct make-init-shapes-cmd (los))
+(define-struct init-shapes-cmd (los))
 (define-struct cmds-until-xy-cond (shp1 minx miny maxx maxy cmd))
 
 ;;All examples written assuming that the shapes are placed in the correct location based on the original pictures, even though they are not
 (define ANIMATION1
-  (let [(circle (make-shape 'ellipse (make-posn 100 100) 'red 5 5))
-        (wall1 (make-shape 'rectangle (make-posn 300 100) 'blue 300 100))
-        (wall2 (make-shape 'rectangle (make-posn 100 300) 'red 100 300))]
+  (let [(circle (make-shape 'ellipse (make-posn 100 100) 'red 5 5 1))
+        (wall1 (make-shape 'rectangle (make-posn 300 100) 'blue 300 100 2))
+        (wall2 (make-shape 'rectangle (make-posn 100 300) 'green 100 300 3))]
   (make-animation 400 400
                   (list
                    (make-init-shapes-cmd (list circle wall1))
+                   (make-move-shape-cmd circle (make-delta 10 10))
                    (make-cmds-until-xy-cond circle 100 95 105 105
                                             (list
-                                             (make-cmd-move-shape-cmd circle (make-delta 3 1))
-                                             (make-display-shape-cmd circle)))
+                                             (make-move-shape-cmd circle (make-delta 3 1))))
                    (make-delete-shape-cmd wall1)
                    (make-cmds-until-xy-cond circle 0 'na 0 'na
                                             (list
-                                             (make-cmd-move-shape-cmd circle (make-delta -3 1))
-                                             (make-display-shape-cmd circle))))
+                                             (make-move-shape-cmd circle (make-delta -3 1)))))
                    )))
 
-(define ANIMATION2
-  (let [(circle (make-ball (make-posn 100 100)))
-        (wall1 (make-wall (make-posn 300 100) 300 100))
-        (wall2 (make-wall (make-posn 100 300) 100 300))]
-    (make-animation 400 400
-                    (list
-                     (make-init-shapes-cmd (list circle))
-                     (make-repeat-until-offscreen
-                      (make-jump-shape-cmd circle))))))
+;(define ANIMATION2
+;  (let [(circle (make-ball (make-posn 100 100)))
+;        (wall1 (make-wall (make-posn 300 100) 300 100))
+;        (wall2 (make-wall (make-posn 100 300) 100 300))]
+;    (make-animation 400 400
+;                    (list
+;                     (make-init-shapes-cmd (list circle))
+;                     (make-repeat-until-offscreen
+;                      (make-jump-shape-cmd circle))))))
 
 (define ANIMATION3
   (let [(circle (make-ball (make-posn 100 100)))
@@ -61,7 +60,7 @@
         (wall2 (make-wall (make-posn 100 300) 100 300))]
     (make-animation 400 400
                     (list
-                     (make-init-shapes-cmd (list ( circle wall1))
+                     (make-init-shapes-cmd (list circle wall1))
                      (make-move-until-collide circle (make-delta 0 4))
                      (make-move-until-collide circle (make-delta 4 0))
                      (make-display-shape-cmd wall2)
@@ -75,27 +74,32 @@
 (define HEIGHT 400)
 (create-canvas WIDTH HEIGHT)
 
-(define vars empty)
+(define shapes empty)
 
 ;; A var is (make-var ID value)
 (define-struct var (ID value))
 
 ;; init-vars: list[values] -> void
-(define (init-var a-list)
-  (set! vars (id-vars a-list 1)))
+(define (init-vars a-list)
+  (set! shapes (map (lambda (shpe) (make-var (shape-ID shpe) shpe)) a-list)))
 
-;; id-vars: list[values] id -> list[vars]
-(define (id-vars a-list id)
-  (cond [(empty? a-list) empty]
-        [(cons? a-list)
-         (append (list (make-var id (first a-list)))
-                 (id-vars (rest a-list) (+ 1 id)))]))
-  
-;; lookup-var-by-id: id -> value
+;; lookup-shape-by-id: id -> value
 (define (lookup-var-by-id id)
-  (first (filter (lambda (item) (= (var-ID item) id)) vars)))
+  (first (filter (lambda (item) (= (var-ID item) id)) shapes)))
+ 
+;; lookup-shape: shape -> shape
+ (define (lookup-shape a-shape)
+   (first (filter (lambda (a-var) (= (var-ID a-var) (shape-ID a-shape))) shapes)))
+
+;; delete-shape: shape -> void
+(define (delete-shape a-shape)
+  (set! shapes (filter (lambda (a-var) (not (= (var-ID a-var) (shape-ID a-shape))))
+                       shapes)))
  
 
+;; add-shape: shape -> void
+(define (add-shape shape)
+  (set! shapes (append shapes (list (make-var (shape-ID shape) shape)))))
   
 ;;run-animation: animation -> void
 (define (run-animation anim)
@@ -113,16 +117,45 @@
 
 ;;run-cmd: cmd -> void
 (define (run-cmd cmd)
-  (cond [(display-shape-cmd? cmd)]
-        [(delete-shape-cmd? cmd)]
-        [(move-shape-cmd? cmd)]
-        [(jump-shape-cmd? cmd)]
-        [(make-init-shapes-cmd? cmd)]
-        [(cmds-until-xy-cond? cmd)]))
+  (cond [(add-shape-cmd? cmd) (add-shape (add-shape-cmd-shape cmd))]
+        [(delete-shape-cmd? cmd) (delete-shape (delete-shape-cmd-shape cmd))]
+        [(move-shape-cmd? cmd) (move-shape (move-shape-cmd-shape cmd) (move-shape-cmd-delta cmd))]
+        [(jump-shape-cmd? cmd) (jump-shape (jump-shape-cmd-shape cmd))]
+        [(init-shapes-cmd? cmd) (init-vars (init-shapes-cmd-los cmd))]))
+        ;[(cmds-until-xy-cond? cmd) (move-until-xy cmd)]
+
+
+
+
+;;move-shape: shape delta -> void
+(define (move-shape a-shape a-delta)
+  (edit-shape-posn a-shape (delta-x a-delta) (delta-y a-delta)))
+
+;;jump-shape: shape -> void
+(define (jump-shape a-shape)
+  (edit-shape-posn a-shape random(WIDTH) random(HEIGHT)))
+
+;;edit-shape-posn: shape number number -> void
+(define (edit-shape-posn a-shape dx dy)
+    (delete-shape a-shape)
+    (add-shape (make-shape (shape-type a-shape)
+                           (let ([x (posn-x (shape-posn a-shape))]
+                                 [y (posn-y (shape-posn a-shape))])
+                             (make-posn (+ x dx) (+ y dy)))
+                           (shape-color a-shape)
+                           (shape-width a-shape)
+                           (shape-height a-shape)
+                           (shape-ID a-shape))))
+
+;;move-until-xy: cmd -> void
+;(define (move-until-xy cmd)
+ 
 
 ;;show-all: -> void
 (define (show-all)
-  (update-frame (show-shapes (filter (lambda (item) (shape? item)) vars))))
+  (update-frame (show-shapes (map (lambda (item) (cond [(shape? (var-value item))
+                                                        (var-value item)]
+                                                       [else empty])) shapes))))
 
 ;;show-shapes: list[shape] -> scene
 (define (show-shapes los)
@@ -138,17 +171,19 @@
 
 ;;make-shape-img: shape -> image
 (define (make-shape-img shape)
-  (shape-type shape) (shape-width width) (shape-height shape) (shape-color shape)
   (let [(wid (shape-width shape))
         (hei (shape-height shape))
-        (col (shape-color col))]
-  (cond [(symbol=? 'triangle)
+        (col (shape-color shape))]
+  (cond [(symbol=? 'triangle (shape-type shape))
          (isosceles-triangle (sqrt (+ (expt hei 2) (expt (* .5 wid) 2)))
                              (* 2 (tan (/ (* .5 (shape-width shape)) (shape-height))))
                              'solid col)]
-        [(symbol=? 'ellipse)
+        [(symbol=? 'ellipse (shape-type shape))
          (ellipse wid hei 'solid col)]
-        [(symbol=? 'rectangle)
+        [(symbol=? 'rectangle (shape-type shape))
          (rectangle wid hei 'solid col)])))
-         
+
+
+(run-animation ANIMATION1)
+
   
